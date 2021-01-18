83e26552cbc4bfd3862a0fae0529f0008a19b404
ory-r <oooory@gmail.com>
2021-01-18 12:26:48 +0200

    change on way buttons work in sideBar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,32 +4,32 @@ import { Button, Container, Row } from "react-bootstrap";
 
 import EnterWageSection from "./EnterWageSection";
 import EditMembers from "./EditMembers";
+import WagesSection from "./WagesSection";
 
 function Sidebar(props) {
-    const [showEnterWage,setShowEnterWage]= useState(false)
-    const [showMemberSection, setShowMemberSection]= useState(false)
-    const [showWageSection, setShowWageSection] = useState(false);
+    // const [showEnterWage,setShowEnterWage]= useState(false)
+    const [showSection, setShowSection]= useState(null)
+    // const [showWageSection, setShowWageSection] = useState(false);
     
 
     function addWage(){
         alert("כאן תוקם קריאת שרת מפוארת")
     }
-    
+    const sections= [<EnterWageSection addWage={addWage}/>, <EditMembers />, <WagesSection/>]
+
     return(
         <Container>
             <Row className="container">
                 <div className="sidebar col-sm-3">
                     <Button variant="info" onClick={()=>
-                        setShowEnterWage(showEnterWage== false ? true: false)}> הזן שכר</Button>
+                        setShowSection(0)}> הזן קובץ שכר</Button>
                     <Button variant="info" onClick={()=>
-                        setShowMemberSection(showMemberSection== false ? true: false)}> ערוך חברים</Button>
-                    <Button  variant="info" onClick="#"> הצג שכר</Button>
-                    
+                        setShowSection(1)}> ערוך חברים</Button>
+                    <Button  variant="info" onClick={()=>
+                        setShowSection(2)}> הצג שכר</Button>
                 </div>
                 <div className= "main-section col-sm-9">
-                    {showMemberSection? <EditMembers/>: null}
-                    {showEnterWage?<EnterWageSection  addWage={addWage}/>: null}
-                    {/* {showWageSection? <WagesSection/>:null} */}
+                    {sections[showSection]}
                 </div>
             </Row>
         </Container>
