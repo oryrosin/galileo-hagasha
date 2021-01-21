c6b90a20e3b4bb013b0b77706487a72e1eef3cf5
ory-r <oooory@gmail.com>
2021-01-21 16:39:06 +0200

    Wage section shows table from api and export excel file

---
+++ src/Models/wageModel.js
@@ -0,0 +1,21 @@
+
+class WageModel {
+    constructor(fields, id){
+            this.fName= fields.fName;
+            this.lName= fields.lName;
+            this.month= fields.month;
+            this.precentage= fields.precentage;
+            this.idNum= fields.idNum;
+            this.socialSecutrity= fields.socialSecutrity;
+            this.hitagdut= fields.hitagdut;
+            this.year= fields.year;
+            this.bruto= fields.bruto;
+            this.neto= fields.neto;
+            this.employerCost= fields.employerCost;
+            this.pension= fields.pension;
+            this.gamish= fields.gamish;
+            this.membersTax= fields.membersTax;
+            this.id=id;
+        }   
+    }
+export default WageModel
--- src/components/WagesSection.js
+++ src/components/WagesSection.js
@@ -1,59 +1,125 @@
-import { useState } from "react"
-import { Form, Row, Col, Button} from "react-bootstrap"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Form, Row, Col, Button, Table } from "react-bootstrap"
+import WageModel from "../Models/wageModel"
 import './WagesSection.css'
+import { FaFileExcel } from 'react-icons/fa';
+import ReactHTMLTableToExcel from 'react-html-table-to-excel';
 
 
-function WagesSection(props){
+
+function WagesSection(props) {
     const [filter1, setFilter1] = useState("")
     const [filter2, setFilter2] = useState("")
     const [filter3, setFilter3] = useState("")
     const [filter4, setFilter4] = useState("")
     const [filter, setFilter] = useState("")
+    const [wagesData, setWagesData] = useState(null)
     // axios get
-
-    useEffect(()=>{
-        async function getWagesData(){
-            const res= await axios.get("https://api.airtable.com/v0/appivINepijXjwR9W/Table%203?api_key=keyk7ppRxdcVwPFzd");
-            const members= res.data.records.map((plainMember) => new MemberModel(plainMember.fields, plainMember.id));
-            setMembersData(members);
-        }
-        editSelectedMember() ;
-        },[showModal]);
     // filters by onChange
     // return table of members each is clickble for wages view
-    
-    return(
-        <Form id="wage-section-form">
-        <Form.Group as={Row} controlId="formHorizontalEmail">
-            <Form.Label column sm={2}> התאגדות</Form.Label>
-            <Col sm={4}><Form.Control type="text" 
-            value={filter} onChange={(e)=> setFilter1(e.target.value)}/></Col>
-        </Form.Group>
-        <Form.Group as={Row} controlId="formHorizontalEmail">
-            <Form.Label column sm={2}>מעגל חיים</Form.Label>
-            <Col sm={4}><Form.Control type="text" /></Col>
-        </Form.Group>
-        <Form.Group as={Row} controlId="formHorizontalEmail">
-            <Form.Label column sm={2}>חבר</Form.Label>
-            <Col sm={4}><Form.Control type="text" /></Col>
-        </Form.Group>
-        <Form.Group as={Row} controlId="formHorizontalEmail">
-            <Form.Label column sm={2}>מתאריך</Form.Label>
-            <Col sm={4}><Form.Control type="text" /></Col>
-            <Form.Label column sm={2}>עד תאריך</Form.Label>
-            <Col sm={4}><Form.Control type="text" /></Col>
-        </Form.Group>
-        <Form.Group as={Row} controlId="formHorizontalEmail">
-            <Form.Label column sm={2}>מעסיק</Form.Label>
-            <Col sm={4}><Form.Control type="text" /></Col>
-        </Form.Group>
-        <Form.Group as={Row} controlId="formHorizontalEmail">
-            <Form.Label column sm={2}>אג"ש</Form.Label>
-            <Col sm={4}><Form.Control type="text" /></Col>
-        </Form.Group>
-        
-        
-        </Form>
+    useEffect(() => {
+        async function getWagesData() {
+            const res = await axios.get("https://api.airtable.com/v0/appivINepijXjwR9W/Table%203?api_key=keyk7ppRxdcVwPFzd");
+            const wagesRows = res.data.records.map((row) => new WageModel(row.fields, row.id));
+            setWagesData(wagesRows);
+        }
+        getWagesData();
+    }, []);
+
+    let wagesToShow;
+    if (wagesData !== null) {
+        wagesToShow = wagesData.map((wage) =>
+            <tr>
+                <td>{wage.fName}</td>
+                <td>{wage.lName}</td>
+                <td>{wage.lName}</td>
+                <td>{wage.lName}</td>
+                <td>{wage.lName}</td>
+                <td>{wage.lName}</td>
+                <td>{wage.lName}</td>
+                <td>{wage.lName}</td>
+                <td>{wage.lName}</td>
+                <td>{wage.lName}</td>
+                <td>{wage.lName}</td>
+                <td>{wage.lName}</td>
+                <td>{wage.lName}</td>
+                <td><FaFileExcel /></td>
+                <td><FaFileExcel /></td>
+                <td><FaFileExcel /></td>
+            </tr>
+        ); console.log(wagesData)
+    }
+
+    return (
+        <>
+            <Form id="wage-section-form">
+                <Form.Group as={Row} controlId="formHorizontalEmail">
+                    <Form.Label column sm={2}> התאגדות</Form.Label>
+                    <Col sm={4}><Form.Control type="text"
+                        value={filter} onChange={(e) => setFilter1(e.target.value)} /></Col>
+                </Form.Group>
+                <Form.Group as={Row} controlId="formHorizontalEmail">
+                    <Form.Label column sm={2}>מעגל חיים</Form.Label>
+                    <Col sm={4}><Form.Control type="text" /></Col>
+                </Form.Group>
+                <Form.Group as={Row} controlId="formHorizontalEmail">
+                    <Form.Label column sm={2}>חבר</Form.Label>
+                    <Col sm={4}><Form.Control type="text" /></Col>
+                </Form.Group>
+                <Form.Group as={Row} controlId="formHorizontalEmail">
+                    <Form.Label column sm={2}>מתאריך</Form.Label>
+                    <Col sm={4}><Form.Control type="text" /></Col>
+                    <Form.Label column sm={2}>עד תאריך</Form.Label>
+                    <Col sm={4}><Form.Control type="text" /></Col>
+                </Form.Group>
+                <Form.Group as={Row} controlId="formHorizontalEmail">
+                    <Form.Label column sm={2}>מעסיק</Form.Label>
+                    <Col sm={4}><Form.Control type="text" /></Col>
+                </Form.Group>
+                <Form.Group as={Row} controlId="formHorizontalEmail">
+                    <Form.Label column sm={2}>אג"ש</Form.Label>
+                    <Col sm={4}><Form.Control type="text" /></Col>
+                    <Col sm={3}></Col> 
+                    <ReactHTMLTableToExcel
+                    id="test-table-xls-button"
+                    className="download-table-xls-button"
+                    table="table-to-xls"
+                    filename="galileo-data-4u"
+                    sheet="tablexls"
+                    buttonText="הורד כקובץ Excel" />
+                </Form.Group>
+                
+            </Form>
+            <div>
+                <br />
+                <Table responsive striped bordered hover variant="dark" id="table-to-xls">
+                    <thead>
+                        <tr>
+                            <td>שם פרטי</td>
+                            <td>שם משפחה</td>
+                            <td> ת.ז</td>
+                            <td>חודש</td>
+                            <td>שנה</td>
+                            <td>גמי"ש</td>
+                            <td>ברוטו</td>
+                            <td> נטו</td>
+                            <td>עלות</td>
+                            <td>ביטוח לאומי</td>
+                            <td>פנסיה</td>
+                            <td>אחוז משרה</td>
+                            <td>דמי חבר</td>
+                            <td>הצג תלוש</td>
+                            <td>הצג תלוש</td>
+                            <td>הצג תלוש</td>
+                        </tr>
+                    </thead>
+                    <tbody> {wagesToShow} </tbody>
+                </Table>
+                <br />
+                
+            </div>
+        </>
 
 
         // <Form id="wage-section-form">
@@ -69,9 +135,9 @@ function WagesSection(props){
         //     <input type="text" id="filter-by-gamish" 
         //     // value={filter} onChange={(e)=> setFilter3(e.target.value)}
         //     /><br/>
-            
+
         // </Form>
-        
+
     )
 }
 
